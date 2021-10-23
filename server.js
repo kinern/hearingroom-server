22e9035d51c8f21322e5e8033b2ca80b3348bb36
Natalie <kinern01@gmail.com>
2021-10-23 12:46:24 -0700

    added route for getting all records (currently limited to 10) from DynamoDB database.

--- server.js
+++ server.js
@@ -5,7 +5,44 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/news-reader"));
+
+/* AWS DynamoDB Connection Settings */
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+const client = new AWS.DynamoDB.DocumentClient();
+const tableName = 'news-reader';
+
+//Test route for server.
+app.get("/", (req, res) => {
+  res.json({ message: "The server is running." });
+});
+
+
+
+//Queries... need to seperate out into routes folder
+
+//Get all records - currently limited to 10...
+app.get("/news-reader/all", (req, res) => {
+
+  var params = {
+    TableName: tableName, //"news-reader"
+    Limit: 10
+  };
+
+  client.scan(params, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        var items = [];
+        for (var i in data.Items)
+            items.push(data.Items[i]);
+        res.contentType = 'application/json';
+        res.send(data);
+    }
+  });
+
+});
+
 
 app.listen(port, () => {
 
